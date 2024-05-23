9c8e90fdbc6c2722b4aac179fd079963dbe4d580
partharoyy <69928055+partharoyy@users.noreply.github.com>
2024-05-23 19:14:27 +0530

    Fix vercel deploy error

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 async function connectDB() {
   mongoose
-    .connect(process.env.NEXT_PUBLIC_DATABASE_URI)
+    .connect(process.env.NEXT_PUBLIC_DATABASE_URI.toString())
     .then(() => console.log('DB connected'))
     .catch((e) => console.log(e));
 }
